7918d9c99b2c0f6a4252163dd187a54af80d90f3
Cebrail Akyüz <103183740+Cebrail000001F@users.noreply.github.com>
2022-12-10 19:44:12 +0300

    ++

--- Assets/Scripts/Mermi.cs
+++ Assets/Scripts/Mermi.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class Mermi : MonoBehaviour
 {
-   
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject[] mermiler;
+    [SerializeField] private GameObject dogmaNoktasi;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       
-            if (collision.gameObject.CompareTag("Objeler"))
+        for (int i = 0; i < mermiler.Length; i++)
+        {
+            if (collision.gameObject == mermiler[i])
             {
                 gameObject.SetActive(false);
-               
             }
-        
+        }             
+    }
+    /// <summary>
+    /// Obje havuzunda aktif olmayan ilk objeyi aktif eder
+    /// </summary>
+    void mermiHavuzu()
+    {
+        for (int i = 0; i < mermiler.Length; i++)
+        {
+            if (!mermiler[i].activeInHierarchy)
+            {
+                mermiler[i].transform.position = dogmaNoktasi.transform.position;
+                mermiler[i].SetActive(true);
+                break;
+            }
+
+        }
     }
 }
