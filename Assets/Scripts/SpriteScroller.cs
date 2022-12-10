546ec55828362ee1db26fdc0092851aa9223a6b1
Yasin Uçar <yasinucar@protonmail.com>
2022-12-10 22:24:18 +0300

    background

---
+++ Assets/Scripts/SpriteScroller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScroller : MonoBehaviour
+{
+    [SerializeField] Vector2 _moveSpeed;
+    private Vector2 _offset;
+    private Material _material;
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+
+    void Update()
+    {
+        _offset = _moveSpeed * Time.deltaTime;
+        _material.mainTextureOffset += _offset;
+    }
+}
