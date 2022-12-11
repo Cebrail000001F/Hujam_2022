0d5a96e914ae132f7378b3177e9bc008e2356bc7
Ali-Durman <99588223+Ali-Durman@users.noreply.github.com>
2022-12-11 16:35:37 +0300

    .
    .

---
+++ Assets/Scripts/DestroyingObjects.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyingObjects : MonoBehaviour
+{
+    [SerializeField] private GameObject[] mass;
+}
