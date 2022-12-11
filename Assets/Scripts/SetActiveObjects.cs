3ba858e3cf662f8a2f365f7967887c8479da170d
Ali-Durman <99588223+Ali-Durman@users.noreply.github.com>
2022-12-11 19:12:09 +0300

    .
    .

---
+++ Assets/Scripts/SetActiveObjects.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveObjects : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private void Astreoid()
+    {
+
+    }
+    private void Moon()
+    {
+
+    }
+    private void Earth()
+    {
+
+    }
+    private void GasGaint()
+    {
+
+    }
+}
