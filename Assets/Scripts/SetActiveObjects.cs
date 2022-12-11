93100be1b7744fa9b48d0880c9a67682a54b706e
Ali-Durman <99588223+Ali-Durman@users.noreply.github.com>
2022-12-11 20:06:44 +0300

    Setactive Objets Script
    added setactive objects by scales

--- Assets/Scripts/SetActiveObjects.cs
+++ Assets/Scripts/SetActiveObjects.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class SetActiveObjects : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-
-    private void Astreoid()
-    {
-
-    }
+    [SerializeField] private GameObject[] planets;
     private void Moon()
     {
-
+        if (gameObject.transform.localScale.x == 10f && gameObject.transform.localScale.y == 10f)
+            planets[0].SetActive(true);
+        else
+        {
+            planets[1].SetActive(false);
+            planets[2].SetActive(false);
+        }
     }
     private void Earth()
     {
-
+        if (gameObject.transform.localScale.x == 20f && gameObject.transform.localScale.y == 20f)
+            planets[1].SetActive(true);
+        else
+        {
+            planets[0].SetActive(false);
+            planets[2].SetActive(false);
+        }
     }
     private void GasGaint()
     {
-
+        if (gameObject.transform.localScale.x == 30f && gameObject.transform.localScale.y == 30f)
+            planets[2].SetActive(true);
+        else
+        {
+            planets[0].SetActive(false);
+            planets[1].SetActive(false);
+        }
     }
 }
